ed6b8a2931d5e83a580e02e28502b99e42042f0b
huseyin <huseyinjkilic@gmail.com>
2017-08-01 19:12:12 +0300

    Command line working perfectly

--- zeroCounter/src/main/java/com/huseyin/ZeroCounterApplication.java
+++ zeroCounter/src/main/java/com/huseyin/ZeroCounterApplication.java
@@ -1,12 +1,18 @@
 package com.huseyin;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZeroCounterApplication {
+public class ZeroCounterApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZeroCounterApplication.class, args);
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		System.out.println("Hello world from command line");
 	}
 }
