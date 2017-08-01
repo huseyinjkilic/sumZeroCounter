860bcf6fde48e05b4b05b6fc40e6874ce81cea58
huseyin <huseyinjkilic@gmail.com>
2017-08-01 19:05:56 +0300

    After spring boot project

---
+++ zeroCounter/src/main/java/com/huseyin/ZeroCounterApplication.java
@@ -0,0 +1,12 @@
+package com.huseyin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroCounterApplication.class, args);
+	}
+}
