c74a3bbc76e82c2d5ad1a9f17798f09c7c728f85
huseyin <huseyinjkilic@gmail.com>
2017-08-01 22:05:43 +0300

    Final version before documentation

--- zeroCounter/src/main/java/com/huseyin/ZeroCounterController.java
+++ zeroCounter/src/main/java/com/huseyin/ZeroCounterController.java
@@ -1,7 +1,13 @@
 package com.huseyin;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @RestController
@@ -9,6 +15,55 @@ public class ZeroCounterController {
 
     @RequestMapping("/")
     public String index() {
-        return "Rest from Spring Boot!";
+    	
+		int [] arrayNumbers = {3, -2, 0, 1 , 0,  4, 7, 0, -3, -2, -8};
+		return Integer.toString(calculateCountOfPair(arrayNumbers));
+    }
+    
+    @RequestMapping(value="/data/{arrayValues}", method = RequestMethod.GET)
+    public @ResponseBody String getParameter(@PathVariable("arrayValues") String arrayValues) {
+    	String[] arayValuesFromRest = arrayValues.split(",");
+    	int[] integerValueOfArray = new int[arayValuesFromRest.length]; 
+    	
+    	for(int i = 0; i <arayValuesFromRest.length; i++ ) {
+    		integerValueOfArray[i] = Integer.parseInt(arayValuesFromRest[i]);
+    	}
+    	
+    	return Integer.toString(calculateCountOfPair(integerValueOfArray));
+    	
+    }
+    
+    public int calculateCountOfPair(int[] numbers) {
+		Arrays.sort(numbers);
+		
+		int countOfEqualNumber = 0;
+		int currentNegativeNumber = 0;
+		int currentPositiveNumber = numbers.length - 1;
+		
+		while(currentNegativeNumber != currentPositiveNumber) {
+			
+			if(numbers[currentNegativeNumber] + numbers[currentPositiveNumber] != 0 ) {
+				if((numbers[currentNegativeNumber] <= 0)  && (numbers[currentPositiveNumber] >= 0) )  {
+					if(Math.abs(numbers[currentNegativeNumber]) > numbers[currentPositiveNumber]) {
+						currentNegativeNumber++;
+					} else if(Math.abs(numbers[currentNegativeNumber]) < numbers[currentPositiveNumber]) {
+						currentPositiveNumber--;
+					}
+				} else {
+					break;
+				}
+			} else {
+				countOfEqualNumber++;
+				
+				// This is for middle values. If we don't check this value for middle. 
+				// It might be swap for equal number and double count.
+				if(Math.abs(currentNegativeNumber - currentPositiveNumber) == 1) {
+					break;
+				}
+				currentNegativeNumber++;
+				currentPositiveNumber--;
+			}
+		}
+        return countOfEqualNumber;
     }
 }
--- zeroCounter/src/test/java/com/huseyin/ZeroCounterApplicationTests.java
+++ zeroCounter/src/test/java/com/huseyin/ZeroCounterApplicationTests.java
@@ -27,10 +27,38 @@ public class ZeroCounterApplicationTests {
     private MockMvc mvc;
 
     @Test
-    public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+    public void getFour() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/data/3,-2,0,1,0,4,7,0,-3,2,-8,8").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("Rest from Spring Boot!")));
+                .andExpect(content().string(equalTo("4")));
+    }
+    
+    @Test
+    public void getThree() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/data/3,-2,0,1,0,2,-3").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("3")));
+    }
+    
+    @Test
+    public void getZero() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/data/1,4,7,-3,2,-8").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("0")));
+    }
+    
+    @Test
+    public void getZeroForShortArray() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/data/0").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("0")));
+    }
+    
+    @Test
+    public void getOne() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/data/0,0,0").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("1")));
     }
 
 }
